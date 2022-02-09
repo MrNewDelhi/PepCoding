e0682064b0105400b7d481e5f8162ac1dd0c8c5c
anmol <anmolsoin1@gmail.com>
2022-02-09 05:32:59 +0530

    Mentored by Abhishek-Pepcoding

---
+++ POC/Javascript/Lec_1/index.js
@@ -0,0 +1,18 @@
+var a;
+
+a = 10;
+console.log(typeof a);
+console.log(a);
+
+a = "hello";
+console.log(a);
+
+console.log(typeof a);
+
+a = true;
+console.log(a);
+console.log(typeof a);
+
+a = null;
+console.log(a);
+console.log(typeof a);
